5392d1788335b9d2cc75491b28b7e20c60bb28ca
divyesh-bhalala <dsbhalala99@gmail.com>
2015-05-30 20:03:59 +0530

    add config

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+/* configuration */
+ini_set("display_errors", "1");
+error_reporting(E_ALL);
+define('CONSUMER_KEY', 'pXWaxgrWhLa8YZn7YVcgpbvCs');
+define('CONSUMER_SECRET', '1v8DvZprFdeFMVam957w90EwlFnOzZllgXMJrDtIcGiX6ifR7C');
+define('OAUTH_CALLBACK', 'http://twittery-rttweet.rhcloud.com/redirect.php');
+?>
