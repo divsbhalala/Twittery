33f3d057e08883ed0b175153a20988413c05251b
divyesh bhalala <bhalaladivs@gmail.com>
2015-06-10 18:53:06 +0530

    update download

--- ForceFile.php
+++ ForceFile.php
@@ -4,7 +4,6 @@ $filename=explode('/', $ff);
 $file=$filename[count($filename)-1];
 header ("Content-type: octet/stream");
 header ("Content-disposition: attachment; filename=".$file.";");
-header("Content-Length: ".filesize($ff));
-readfile($_REQUEST['file']);
+readfile($ff);
 exit;
 ?>
