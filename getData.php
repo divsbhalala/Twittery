41c8eb2cfc5c4c723b8c0d4b7cbd3a0080d8f397
divyesh-bhalala <dsbhalala99@gmail.com>
2015-06-11 09:07:30 +0530

    optimize tweet

--- getData.php
+++ getData.php
@@ -29,7 +29,19 @@ if (isset($request->tweetUserName)) {
 
 if (!empty($tweetUserName) && !empty($tweetType)) {
     /* --------get followers tweet-------------- */
-    $data = $conn->get_tweet($tweetType, $tweetUserName,$oauth_token,$oauth_token_secret);
+     if($tweetUserName=='me'){
+        if(!isset($_SESSION['alltweetData']) && empty($_SESSION['alltweetData'])){
+            $data = $conn->get_tweet($tweetType, $tweetUserName,$oauth_token,$oauth_token_secret);
+        }
+        else{
+           $data=array_slice($_SESSION['alltweetData'],0,10);
+        }
+
+    }
+    else{
+        $data = $conn->get_tweet($tweetType, $tweetUserName,$oauth_token,$oauth_token_secret);
+    }
+
     $_SESSION['tweetData']=$data;
     echo json_encode($data);
     return;
