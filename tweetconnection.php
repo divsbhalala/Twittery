dd8b05cd2596415c0453d93cf59935ee638f1fa1
divyesh bhalala <bhalaladivs@gmail.com>
2015-06-11 22:13:07 +0530

    remove comment config

--- tweetconnection.php
+++ tweetconnection.php
@@ -9,9 +9,6 @@ require_once('lib/php-export-data.class.php');
 
 
 require_once('config.php');
-/*define('CONSUMER_KEY', 'pXWaxgrWhLa8YZn7YVcgpbvCs');
-define('CONSUMER_SECRET', '1v8DvZprFdeFMVam957w90EwlFnOzZllgXMJrDtIcGiX6ifR7C');
-define('OAUTH_CALLBACK', 'http://twittery-rttweet.rhcloud.com/redirect.php');*/
 
 class tweetconnection {
 
